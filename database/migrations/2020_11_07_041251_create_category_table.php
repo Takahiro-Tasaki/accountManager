3f966b3dba23cb07f397b9d07bac14529a2109b2
田﨑貴大 <tasaki_takahiro@manabito.biz>
2020-11-21 12:12:07 +0900

    2020.11.07 commit.

---
+++ database/migrations/2020_11_07_041251_create_category_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+          $table->increments('category_id');
+          $table->string('name', 40);
+          $table->unsignedInteger('parent_id');
+          $table->timestamp('create_date')->useCurrent();
+          $table->timestamp('update_date')->useCurrent();
+          $table->timestamp('delete_date')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
---
+++ database/migrations/2020_11_07_045450_create_account_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account', function (Blueprint $table) {
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('account_id');
+            $table->text('url');
+            $table->string('login_id', 256);
+            $table->string('password', 256);
+            $table->unsignedInteger('kind_id');
+            $table->text('note');
+            $table->timestamp('create_date')->useCurrent();
+            $table->timestamp('update_date')->useCurrent();
+            $table->timestamp('delete_date')->useCurrent();
+            
+            $table->primary(['customer_id', 'account_id']);
+            $table->foreign('customer_id')->references('customer_id')->on('customer');
+            $table->foreign('kind_id')->references('kind_id')->on('kind');
+        });
+        
+        Schema::table('account', function (Blueprint $table) {
+            $table->increments('account_id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account');
+    }
+}
